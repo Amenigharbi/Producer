fb6f17e6a50866fbf2340880d06535842b7cad49
ameni <gharbiameni17@gmail.com>
2024-04-13 19:23:21 +0100

    nouvelles

--- src/main/java/com/Microservices/ProducerService/AddServiceApplication.java
+++ src/main/java/com/Microservices/ProducerService/AddServiceApplication.java
@@ -2,8 +2,10 @@ package com.Microservices.ProducerService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy
 public class AddServiceApplication {
 
 	public static void main(String[] args) {
